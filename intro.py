ef59c5b2238b7132111224094bf249bcdbce0aea
Tysenxw <twills12@terpmail.umd.edu>
2025-04-07 11:20:13 -0400

    updated intro.py
    Tysens edits to the updated intro.py

--- intro.py
+++ intro.py
@@ -2,3 +2,5 @@ class Family:
     """A family tree"""
     def __init__(self,name):
         self.name = name
+        self.parents = []  # List to store parents - Tysen
+        self.children = []  # List to store children -Tysen
