8b07062fea9f4b3969e62750574e134e8f11488d
Tysenxw <twills12@terpmail.umd.edu>
2025-04-18 10:26:28 -0400

    Luis's function prj_deliverable.py

--- prj_deliverable.py
+++ prj_deliverable.py
@@ -1,4 +1,26 @@
-
+# Luis Function
+#Intended to facilitate the dice roll:
+import random
+def dice_roll():
+    
+    roll_one = random.randint(0, 6)
+    roll_two = random.randint(0, 6)
+    roll_three = random.randint(0, 6)
+    roll_four = random.randint(0,6)
+    total_roll = roll_one + roll_two
+    player2_roll = roll_three + roll_four
+    
+    while (total_roll == player2_roll):
+        
+        
+        if (total_roll != player2_roll):
+            print(total_roll)
+    
+    
+    print(total_roll)
+    print(player2_roll)
+    
+dice_roll()
 # Gerardo - Turn Function:
 # Intended to swap turns after the player performs their move while keeping track of who went. 
 
