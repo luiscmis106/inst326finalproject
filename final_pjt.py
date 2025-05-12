1fd16c56ebba4f2fddedae6e444383f64cbc28a1
Gerardo Sandoval <sandovalgerardo172@gmail.com>
2025-05-11 21:23:57 -0400

    added Luis's functions

--- final_pjt.py
+++ final_pjt.py
@@ -2,6 +2,93 @@ import sys
 import random
 from argparse import ArgumentParser
 
+def dice_roll():
+    """The dice roll for the game
+    
+    Returns:
+        The plyer's dice roll and the opponent's dice roll
+    """
+    
+    roll_one = random.randint(0, 6)
+    roll_two = random.randint(0, 6)
+    roll_three = random.randint(0, 6)
+    roll_four = random.randint(0,6)
+    total_roll = roll_one + roll_two
+    player2_roll = roll_three + roll_four
+    
+    power = {0: "Skip turn", 
+                 2: "Deal 2 extra damage",
+                 4: "Heal 2 HP",
+                 6: "Shield 2 damage",
+                 8: "Opponent skips turn",
+                 10: "Repel next attack",
+                 12: "Take 3 HP from opponent"}
+    
+    if(total_roll == 0 or player2_roll == 0):
+        power.get(0)
+    elif (total_roll == 2 or player2_roll == 2):
+        power.get(2)
+    elif(total_roll == 4 or player2_roll == 4):
+        power.get(4)
+    elif(total_roll == 6 or player2_roll == 6):
+        power.get(6)
+    elif(total_roll == 8 or player2_roll == 8):
+        power.get(8)
+    elif(total_roll == 10 or player2_roll == 10):
+        power.get(10)
+    elif(total_roll == 12 or player2_roll == 12):
+        power.get(12)
+    
+    
+    if (total_roll != player2_roll):
+        return(total_roll, player2_roll)
+    
+    while (total_roll == player2_roll):
+        break
+    return("Same number, roll again!")
+    
+    
+dice_roll()
+
+class player_store:
+    """This is the store where a player can buy powerups based on an odd
+    numbered dice roll"""
+    power_up = {3: "Skip turn", 
+                 5: "Deal 2 extra damage",
+                 7: "Block next attack",
+                 9: "Heal 2 HP",
+                 11: "Shield 2 damage",
+                 13: "Opponent always misses",
+                 15: "Repel next attack",
+                 17: "Take 3 HP from opponent",
+                 19: "Player always hits"}
+    
+    if dice_roll.random == 0:
+        print("You don't have enough points!")
+    else:
+        print("Welcome to the store! What would you like to purchase?")
+        
+    if dice_roll.random == power_up.get(3):
+        print("Would you like to buy the ability to skip one turn?")
+    elif dice_roll.random == power_up.get(5):
+        print("Would you like for your next attack to deal 2 additional damage")
+    elif dice_roll.random == power_up.get(7):
+        print("Would you like to block the next attack?")
+    elif dice_roll.random == power_up.get(9):
+        print("Would you like to instantly restore 2 HP?")
+    elif dice_roll.random == power_up.get(11):
+        print("Would you like to shield the next 2 damage points?")
+    elif dice_roll.random == power_up.get(13):
+        print("Would you like for your opponent to miss their next action?")
+    elif dice_roll.random == power_up.get(15):
+        print("Would you like to deflect the next attack to your enemy?")
+    elif dice_roll.random == power_up.get(17):
+        print("Would you like to drain 3 HP from your enemy?")
+    elif dice_roll.random == power_up.get(19):
+        print("Would you like to attack regardless of your dice roll?")
+        
+        
+
 class Player:
     def __init__(self, name, health, max_health, attack_power=0, heal_power=0, type="warrior"):
         self.name = name
